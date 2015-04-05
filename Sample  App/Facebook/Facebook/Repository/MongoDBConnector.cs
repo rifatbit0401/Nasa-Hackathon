49a7637f869d0a112cba429c85dad48a5437f52e
rifatbit0401 <rifat92iitdu@gmail.com>
2015-04-05 15:16:29 +0600

    sample angular project added

---
+++ Sample  App/Facebook/Facebook/Repository/MongoDBConnector.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Facebook.Model;
+using MongoDB.Driver;
+
+namespace Facebook.Repository
+{
+    class MongoDbConnector
+    {
+        protected MongoCollection Collection;
+        public MongoDbConnector()
+        {
+            Collection =
+                new MongoClient("mongodb://localhost").GetServer().GetDatabase("Facebook").GetCollection<User>("User");
+        }
+    }
+}
